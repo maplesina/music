9858f9e0f1b748997a52a16e0209fc033e12fb7c
qu <836469060maple@sina.com>
2021-04-28 11:14:29 +0800

    msg

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,17 @@ master 主线
 2. 左下角可以看到当前的分支名字，是否是自己的分支
 
 3.选择左侧源代码管理
-4.
+4.点击+号，暂存所有更改
+5.输入需要提交的信息
+6.点击✔，提交
+7.【有可能需要登录GitHub帐号或者配置 邮箱和用户名】
+8. 在master分支上，点击左下角的云朵，需要登录github，给一个token，Copy the token.
+Switch back to VS code.
+Click Signing in to github.com... in the status bar.
+Paste the token and hit enter.
+9. 点击左下角云朵，选择第二个公用项目
+10. 在弹窗的登录页登录github的邮箱和密码
+11. 进度上传后，打开GitHub查看仓库是否存在
 
   </div>
 </template>
