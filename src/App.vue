e5a62dba25078db5d7747803e8b85b17a97e4fe4
qu <836469060maple@sina.com>
2021-04-28 09:28:30 +0800

    第一次提交仓库

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<!-- git -->
+master 主线
+  - development 发展线 (dev)
+    - A 分支线
+    - B 分支线
+    - C
+1. 创建自己的分支：点击左下角分支图标，选择新建分支，输入分支名字，按下回车键
+2. 左下角可以看到当前的分支名字，是否是自己的分支
+
+3.选择左侧源代码管理
+4.
+
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
 </style>
